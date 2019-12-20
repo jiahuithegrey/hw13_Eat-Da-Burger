e3f8776c62496b0f4844d92cfac2d3fa0a9ab51d
Jiahui Wang <wjh4289042@hotmail.com>
2019-12-19 16:14:08 -0800

    db

--- server.js
+++ server.js
@@ -1,13 +1,8 @@
-// *****************************************************************************
 // Server.js - This file is the initial starting point for the Node/Express server.
-//
-// ******************************************************************************
 // *** Dependencies
-// =============================================================
 var express = require("express");
 
 // Sets up the Express App
-// =============================================================
 var app = express();
 var PORT = process.env.PORT || 8080;
 
