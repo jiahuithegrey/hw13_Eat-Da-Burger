76f5dc76bc748e82e28b2a9da03628f5d5391b1d
Jiahui Wang <wjh4289042@hotmail.com>
2019-12-19 16:41:03 -0800

    express

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,35 +1,49 @@
-// *********************************************************************************
 // api-routes.js - this file offers a set of routes for displaying and saving data to the db
-// *********************************************************************************
-
-// Dependencies
-// =============================================================
 
 // Grabbing our models
-
 var db = require("../models");
 
 // Routes
-// =============================================================
 module.exports = function(app) {
-
-  // GET route for getting all of the todos
-  app.get("/api/todos", function(req, res) {
-
+  // GET route for getting all of burgers
+  app.get("/api/burgers", function(req, res) {
+    db.Burger.findAll({}).then(function(result) {
+      res.json(results);
+    });
   });
 
-  // POST route for saving a new todo. You can create a todo using the data on req.body
-  app.post("/api/todos", function(req, res) {
-
+  // POST route for saving a new burger. You can create a burger using the data on req.body
+  app.post("/api/burgers", function(req, res) {
+    db.Burger.create({
+      burger_name: DataTypes.STRING,
+      devoured: DataTypes.BOOLEAN
+    }).then(function(results) {
+      res.json(results);
+    });
   });
 
-  // DELETE route for deleting todos. You can access the todo's id in req.params.id
-  app.delete("/api/todos/:id", function(req, res) {
-
+  // DELETE route for deleting burgers. You can access the burger's id in req.params.id
+  app.delete("/api/burgers/:id", function(req, res) {
+    db.Burger.destroy({
+      where:{
+        id:req.params.id
+      }
+    }).then(function(dbBurger){
+      res.json(dbBurger);
+    });
   });
 
-  // PUT route for updating todos. The updated todo will be available in req.body
-  app.put("/api/todos", function(req, res) {
-
+  // PUT route for updating burgers. The updated burger will be available in req.body
+  app.put("/api/burgers", function(req, res) {
+    db.Burger.update({
+      burger_name: DataTypes.STRING,
+      devoured: DataTypes.BOOLEAN
+    },{
+      where: {
+        id: req.body.id
+      }
+    }).then(function(dbBurger){
+      res.json(dbBurger);
+    });
   });
-};
+}
