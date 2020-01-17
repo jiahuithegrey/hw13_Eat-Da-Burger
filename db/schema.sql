602b8bb5774bd204435bb7312ea381231b2b369b
Jiahui Wang <wjh4289042@hotmail.com>
2020-01-17 11:08:54 -0800

    schema

--- db/schema.sql
+++ db/schema.sql
@@ -1,4 +1,8 @@
--- Drops the todolist if it exists currently --
 DROP DATABASE IF EXISTS burgers_db;
--- Creates the "todolist" database --
 CREATE DATABASE burgers_db;
+USE burgers_db;
+CREATE TABLE burgers(
+    id INTEGER NOT NULL AUTO_INCREMENT,
+    burger_name VARCHAR,
+    devoured BOOLEAN NOT NULL
+);
