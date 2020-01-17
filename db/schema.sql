df8cb563842a6c2e30877a11459ecae9084d9a14
Jiahui Wang <wjh4289042@hotmail.com>
2020-01-17 11:16:56 -0800

    seeds

--- db/schema.sql
+++ db/schema.sql
@@ -1,8 +1,8 @@
-DROP DATABASE IF EXISTS burgers_db;
 CREATE DATABASE burgers_db;
 USE burgers_db;
 CREATE TABLE burgers(
     id INTEGER NOT NULL AUTO_INCREMENT,
     burger_name VARCHAR,
-    devoured BOOLEAN NOT NULL
+    devoured BOOLEAN DEFAULT false,
+    PRIMARY KEY (id)
 );
---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (name) VALUES ("Cheeseburger");
+INSERT INTO burgers (name) VALUES ("Animal Style Burger");
+INSERT INTO burgers (name) VALUES ("Double Double Burger");
