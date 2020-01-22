994ca03918401071d575b48bb36cfe3169cf9e41
Jiahui Wang <wjh4289042@hotmail.com>
2020-01-21 17:48:01 -0800

    updates

--- public/js/frontendscript.js
+++ public/js/frontendscript.js
@@ -1,13 +1,22 @@
-let burgers = [];
-let newBurgerEl = $(".newBurger");
-let burgerContainerEl = $(".burgerContainer");
+let newBurgerEl = $(".new-burger");
+let burgerContainerEl = $(".burger-container");
 
 //add event listeners for getting, deleting and editing burgers
 $("#submit-btn").on("click", addBurger);
 $("#devour-btn").on("click", devourBurger);
 
+ // initial burgers array
+let burgers = [];
+ // Getting burgers from database when page loads
 getBurgers();
 
+//get burgers from the database and updates the view
+function getBurgers(){
+    $.get("/api/burgers",function(data){
+        burgers = data;
+        initializeRows();
+    });
+}
 // resets the burgers displayed with new burgers from the database
 function initializeRows(){
     burgerContainerEl.empty();
@@ -17,32 +26,17 @@ function initializeRows(){
     }
     burgerContainerEl.prepend(rowsToAdd);
 }
-//get burgers from the database and updates the view
-function getBurgers(){
-    $.get("/api/burgers",function(data){
-        burgers = data;
-        initializeRows();
-    });
-}
-//delete a burger when clicking the devour button
-function devourBurger(event){
-    event.stopPropagation();
-    let id = $(this).data("id"); //what does this mean?
-    $.ajax({
-        method:"DELETE",
-        url: "/api/burgers/" + id
-    }).then(getBurgers);
-}
-//makes a burger-item row
+
+//makes a new burger row
 function createNewRow(burger){
     let newBurgerRow = $(
         [
-        "<li class='list-group-item burger-item'>",
+        "<li class='list-group-item new-burger'>",
             "<span>",
             burger.name,
             "</span>",
             "<input type='text' style='display: none;'>",
-            "<button class='delete btn btn-danger'>Devour!</button>",
+            "<button class='delete btn btn-danger' id='devour-btn'>Devour!</button>",
             "</li>"
         ].join("")
     );
@@ -53,6 +47,17 @@ function createNewRow(burger){
       }
       return newBurgerRow;
 }
+
+//delete a burger when clicking the devour button
+function devourBurger(event){
+    event.stopPropagation();
+    let id = $(this).data("id"); //what does this mean?
+    $.ajax({
+        method:"DELETE",
+        url: "/api/burgers/" + id
+    }).then(getBurgers);
+}
+
 //add a new burger into database then update the view
 function addBurger(event){
     event.preventDefault();
